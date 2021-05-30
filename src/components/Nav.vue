b9457dc429c935c11cff58f6f4f814ec76f930fa
clouddawn <1993456181@qq.com>
2021-05-30 16:07:24 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,6 +22,7 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -35,7 +36,7 @@
       flex-direction: column;
     }
     > .item.selected {
-      color: red;
+      color: $color-highlight;
     }
     .icon {
       width: 32px;
