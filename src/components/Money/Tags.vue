31b69bd6f30b30259bded66576cbd36729bc5187
clouddawn <1993456181@qq.com>
2021-06-08 15:10:36 +0800

    收集四个组件的数据到record

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,33 +1,46 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag" :class="{selected: selectedTags.indexOf(tag)>=0}"
+          @click="toggle(tag)"
+      >{{ tag }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Tags'
-  };
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Tags extends Vue {
+    @Prop() dataSource: string[] | undefined;
+    selectedTags: string[] = [];
+
+    toggle(tag: string) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index >= 0) {
+        this.selectedTags.splice(index, 1);
+      } else {
+        this.selectedTags.push(tag);
+      }
+      this.$emit('update:value',this.selectedTags)
+    }
+
+    create() {
+      const name = window.prompt('请输入标签名');
+      if (name === '') {
+        window.alert('标签名不能为空');
+      } else if (this.dataSource) {
+        this.$emit('update:dataSource',
+            [...this.dataSource, name]);
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -53,13 +66,19 @@
       }
 
       > li {
-        background: #d9d9d9;
+        $bg: #d9d9d9;
+        background: $bg;
         height: 24px;
         line-height: 24px;
         padding: 0 16px;
         margin-left: 12px;
         margin-top: 4px;
         border-radius: 12px;
+
+        &.selected {
+          background: darken($bg, 50%);
+          color: white;
+        }
       }
     }
 
