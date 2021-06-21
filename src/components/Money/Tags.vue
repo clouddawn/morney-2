b2ff53b75ec977c761d404b5162600c24f7cfb44
clouddawn <1993456181@qq.com>
2021-06-21 21:11:04 +0800

    Tags.vue 使用 Vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,14 +19,16 @@
   @Component({
     computed: {
       tagList() {
-        // todo
-        // return this.$store.fetchTags();
-        return [];
+        return this.$store.state.tagList;
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
+
+    created() {
+      this.$store.commit('fetchTags');
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -43,8 +45,7 @@
       if (!name) {
         return window.alert('标签名不能为空');
       }
-      // todo
-      // store.createTag(name);
+      this.$store.commit('createTag',name);
     }
   }
 </script>
